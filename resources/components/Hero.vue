a40ba340280ec3305ce7f76584591986b34c3b2e
Stefano Ruth <stefano6262@gmail.com>
2017-09-13 18:43:48 +0200

    Change hero module

--- resources/components/Hero.vue
+++ resources/components/Hero.vue
@@ -2,8 +2,7 @@
     <section class="hero is-primary is-fullheight bg-image" :style="{'background-image': 'url('+section.image+')'}">
         <div class="hero-body">
             <div class="container has-text-centered is-uppercase">
-                <h1 class="title">Varming Savværk</h1>
-                <h2 class="subtitle">Alt inden for træarbejde</h2>
+                <img :src="section.logo" class="hero-center-logo">
             </div>
         </div>
     </section>
